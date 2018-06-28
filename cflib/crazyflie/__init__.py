3c8bfdc5ee26c389743c2ef93f063fd29ee80f1c
TL <>
2018-06-28 13:50:54 -0700

    Now prints data when packet sends ~data~

--- cflib/crazyflie/__init__.py
+++ cflib/crazyflie/__init__.py
@@ -56,7 +56,9 @@ __author__ = 'Bitcraze AB'
 __all__ = ['Crazyflie']
 
 logger = logging.getLogger(__name__)
-
+code = '~'
+buf = ''
+recording = 0
 
 class State:
     """Stat of the connection procedure"""
@@ -401,8 +403,14 @@ class _IncomingPacketHandler(Thread):
                 if cb.port != 0xFF:
                     found = True
 
-            if 'LOL' in pk.data:
-                print(pk.data)
+            for i in range(len(pk.data)):
+                if pk.data[i] == code:
+                    recording = 1 - recording #0->1, 1->0, start/stop recording
+                    if (recording == 0):
+                        print(buf) #stopped recording, dump the contents 
+                        buf = ''
+                elif recording:
+                    buf += pk.data[i]
 
             if not found:
                 pass
